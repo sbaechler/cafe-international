8eb38a831f4dfededcf0dcab3988b81d904334da
Simon Bächler <simon@stellanera.com>
2015-03-29 20:13:08 +0200

    Initial setup

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Optimization;
+using System.Web.Optimization.React;
+
+namespace CafeInternational.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+            bundles.UseCdn = true;   //enable CDN support
+            BundleTable.EnableOptimizations = true;
+
+            // Add React.js
+            var react = new ScriptBundle("~/bundles/react", "//fb.me/react-with-addons-0.13.1.min.js")
+            .Include("~/Content/lib/react/react-with-addons.min.js");
+            react.CdnFallbackExpression = "window.React";
+            bundles.Add(react);
+
+            bundles.Add(new JsxBundle("~/bundles/main").Include(
+                // Add your JSX files here
+                "~/Content/jsx/main.jsx"
+                // You can include regular JavaScript files in the bundle too
+                // "~/Content/ajax.js"
+            ));
+        }
+
+    }
+}
---
+++ Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Optimization;
+using CafeInternational.App_Start;
+
+namespace CafeInternational
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
