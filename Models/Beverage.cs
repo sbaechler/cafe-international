099175d7d00aad2f5cd77b28781a526cc98a6f86
Simon Bächler <simon@stellanera.com>
2015-03-29 21:14:49 +0200

    Add first models.

---
+++ Models/Beverage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using CafeInternational.Properties;
+
+namespace CafeInternational.Models
+{
+
+    /// <summary>
+    /// The beverage object. E.g. Cappucino.
+    /// </summary>
+    public class Beverage
+    {
+        private const int MAX_STRENGTH = 10;
+
+        public int ID { get; set; }
+
+        /// <summary>
+        /// The name of the beverage.
+        /// </summary>
+        public String Name { get; set; }
+
+        /// <summary>
+        /// The URL part.
+        /// </summary>
+        public String Slug { get; set; }
+
+        /// <summary>
+        /// The strength
+        /// </summary>
+        [Range(1, MAX_STRENGTH)]
+        public int Strength { get; set; }
+        public String Comment { get; set; }
+        public int CupID { get; set; }
+
+        public virtual ICollection<Country> Countries { get; set; } 
+    }
+}
---
+++ Models/Cup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CafeInternational.Models
+{
+
+    /// <summary>
+    /// The containment of the beverage. A cup or a glass.
+    /// </summary>
+    public class Cup
+    {
+        private const int MAX_SIZE = 300; //ml
+        public int ID { get; set; }
+
+        /// <summary>
+        /// The name of the containment.
+        /// </summary>
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The size in ml.
+        /// </summary>
+        [Required, Range(1, MAX_SIZE)]
+        public int SizeMl { get; set; }
+
+        /// <summary>
+        /// A slug field for use in frontend code.
+        /// </summary>
+        [Required, MaxLength(10)]
+        public string Asset { get; set; }
+
+        /// <summary>
+        /// The lookup table for the frontend rendering.
+        /// </summary>
+        [Required]
+        public string LUT { get; set; }
+    }
+}
