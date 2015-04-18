98968f1f1727e3193bc0211ccd744084772ed080
Simon Bächler <simon@stellanera.com>
2015-04-18 19:45:10 +0200

    LUT function test

--- Content/__tests__/beverage-test.js
+++ Content/__tests__/beverage-test.js
@@ -10,15 +10,53 @@ describe('Espressos', function(){
   var Beverage = require('../components/Beverage.jsx');
   var TestUtils = React.addons.TestUtils;
 
-  it('can make a good espresso', function() {
+  var beverage = {"Countries":[{"BeverageID":2,"CountryISO2":"CH","Popularity":5,"Name":"Espresso","Language":"de-ch"},{"BeverageID":2,"CountryISO2":"CH","Popularity":5,"Name":"Espresso","Language":"fr-ch"}],"Ingredients":[{"Ingredient":{"ID":1,"Name":"espresso","Slug":"espresso"},"Position":1,"AmountMl":30,"AmountOz":1}],"ID":2,"Name":"Espresso","Slug":"espresso","Strength":8,"Comment":null,"CupID":1,
+    cup: {"ID":1,"Name":"Demitasse","Slug":"demitasse","SizeMl":90,"LUT":"[[20, 29, 1.45], [30, 43, 1.4], [60, 78, 1.16],[90, 104, 0.866]]"}};
 
-    //  Render a Beverage
-    var beverage = {"Countries":[{"BeverageID":2,"CountryISO2":"CH","Popularity":5,"Name":"Espresso","Language":"de-ch"},{"BeverageID":2,"CountryISO2":"CH","Popularity":5,"Name":"Espresso","Language":"fr-ch"}],"Ingredients":[{"Ingredient":{"ID":1,"Name":"espresso","Slug":"espresso"},"Position":1,"AmountMl":30,"AmountOz":1}],"ID":2,"Name":"Espresso","Slug":"espresso","Strength":8,"Comment":null,"CupID":1,
-                    cup: {"ID":1,"Name":"Demitasse","Slug":"demitasse","SizeMl":90,"LUT":"[[20, 29, 1.45], [30, 43, 1.43], [60, 78, 1.3],[90, 104, 1.15]]"}};
+  //  Render a Beverage
+  var espresso = TestUtils.renderIntoDocument(
+      <Beverage beverage={beverage} />
+  );
 
-    var espresso = TestUtils.renderIntoDocument(
-        <Beverage beverage={beverage} />
-    );
-    expect(true).toBe(true);
+  it('can make a simple espresso', function() {
+    // check the fill function for a single ingredient
+    var beverageHeight = espresso.getBeverageHeight();
+    var heights = beverageHeight(30);
+    expect(heights).toEqual([0, 43]);
   });
+
+  it('can calculate the fill function for multiple ingredients at known levels', function() {
+    var beverageHeight = espresso.getBeverageHeight();
+    var heights = beverageHeight(20);
+    expect(heights).toEqual([0, 29]);
+    heights = beverageHeight(10);
+    expect(heights).toEqual([29, 43]);
+    heights = beverageHeight(30);
+    expect(heights).toEqual([43, 78]);
+    heights = beverageHeight(30);
+    expect(heights).toEqual([78, 104]);
+  });
+
+  it('can calculate the fill function for multiple ingredients at unknown levels.', function(){
+    var beverageHeight = espresso.getBeverageHeight();
+    var heights = beverageHeight(0);
+    expect(heights).toEqual([0, 0]);
+
+    //                    0,  10,   20, 30, 40,   50,   60, 70,    80,    90
+    var expectedHeights = [0, 14.5, 29, 43, 54.6, 66.2, 78, 86.66, 95.32, 104];
+    for(var i=0; i<(expectedHeights.length-1); i++) {
+      heights = beverageHeight(10);
+      expect(heights).toEqual([expectedHeights[i], expectedHeights[i+1]]);
+    }
+
+  });
+
+  it('can calculate the fill function for multiple ingredients and go past a data point', function(){
+    var beverageHeight = espresso.getBeverageHeight();
+    var heights = beverageHeight(10);
+    expect(heights).toEqual([0, 14.5]);
+    heights = beverageHeight(40);
+    expect(heights).toEqual([14.5, 66.2]);
+  });
+
 });
--- Content/components/Beverage.jsx
+++ Content/components/Beverage.jsx
@@ -14,30 +14,47 @@ var Beverage = React.createClass({
    * Calculates the height of the beverage.
    * Stores the previous height on successive calls.
    * @param {number} amount - Amount in ml
-   * @return {number} height in Pixels.
+   * @return {Array} base line height, div height in Pixels.
    */
   getBeverageHeight: function() {
-    var currentHeightPx = 0,
-        currentAmountMl = 0,
+    var currentHeightPx = 0, // store the current height of the beverage in the cup.
+        currentAmountMl = 0, // store the amount of liquid in the cup.
         LUT = JSON.parse(this.props.beverage.cup.LUT);
     return function(amount){
-      var lastAmount = currentAmountMl,
-          lastIngredient = currentAmountMl;
-      _.forEach(LUT, function(val) {
+      //console.log('current Amount: ', currentAmountMl, ' adding: ', amount, 'ml.');
+      //console.log('current height: ' + currentHeightPx+ 'Px.');
+      var lastVal = [currentAmountMl, currentHeightPx],
+          // store the previous value to return it as base height.
+          lastHeightPx = currentHeightPx,
+          dx=1, dy=0;
+      _.forEach(LUT, function(val, i) {
+        //console.log(''+i+':Traversing LUT at ' + val[0]);
         if(val[0] === (amount + currentAmountMl)) {
+          // the amount is a data point
           currentHeightPx = val[1];
-          currentAmountMl += amount;
+          currentAmountMl = val[0];
+          //console.log('value found.', val[0]);
           return false;
-        } else if (val[0] > currentAmountMl && val[0] < (amount + currentAmountMl)) {
-          currentHeightPx = val[1];
-          lastAmount = val[0];
-        } else if (val[0] > currentAmountMl && val[0] > (amount + currentAmountMl)) {
-          currentHeightPx += val[2] * (amount + currentAmountMl - lastAmount);
+        } else if (val[0] >= currentAmountMl && val[0] < (amount + currentAmountMl)) {
+          // the data point is at or above the current amount but below the new amount:
+          // store the amount for future calculation.
+          lastVal = val;
+          //console.log('storing ', val);
+        } else if (val[0] > currentAmountMl && val[0] >= (amount + currentAmountMl)) {
+          // the amount passed the data point: calculate the difference to the last known value.
+          dx = currentAmountMl + amount - lastVal[0];
+          dy =  val[2] * dx;
+          //console.log('passed height of ', amount+currentAmountMl);
+          //console.log('dx: ', dx, ' dy: ', dy);
+          // store the last known y-value plus the calculated dy.
+          currentHeightPx = lastVal[1] + dy;
           currentAmountMl += amount;
+          //console.log('new values: amount: ', currentAmountMl, 'height ', currentHeightPx);
           return false;
         }
       });
-      return [lastIngredient, currentHeightPx];
+      //console.log('returning ', lastHeightPx, currentHeightPx);
+      return [lastHeightPx, currentHeightPx];
     };
   },
 
@@ -48,10 +65,12 @@ var Beverage = React.createClass({
         beverageHeight = this.getBeverageHeight(),
         fillState = 0;
     cssClasses = classNames('beverage', cup);
+    // create the HTML divs with the correct height.
     var fillUp = _.map(beverage.Ingredients, function(hasIngredient) {
       var heights = beverageHeight(hasIngredient.AmountMl);
       fillState = _.max([fillState, heights[1]]);
       return(<div className={classNames('ingredient', hasIngredient.Ingredient.Slug)}
+                  key={hasIngredient.Position}
                   style={{height: heights[1]+'px', bottom: heights[0]+'px'}}>
       </div>);
     });
