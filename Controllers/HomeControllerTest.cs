4e1a9e2d385d67566662e5b41a1db9eb1f6fa15b
Simon Bächler <simon@stellanera.com>
2015-05-01 23:23:28 +0200

    add controller tests

---
+++ Controllers/HomeControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Mvc;
+using CafeInternational.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CafeInternational.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            var homeController = new HomeController();
+            ActionResult response = homeController.Index();
+            Assert.IsNotNull(response);
+            Assert.IsTrue(response is ViewResult);
+        }
+    }
+}
